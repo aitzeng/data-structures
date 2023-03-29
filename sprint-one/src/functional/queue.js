d77cb291ba53e590892f0e85f96881f0825d5e94
aitzeng <aitzeng@gmail.com>
2023-03-29 16:16:50 -0700

    Complete Functional Pattern Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,18 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[Object.keys(storage)[someInstance.size() - 1] + 1] = value;
   };
 
   someInstance.dequeue = function() {
+    var removedKey = (Object.keys(storage)[0]);
+    var removedStr = storage[removedKey];
+    delete storage[removedKey];
+    return removedStr;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
