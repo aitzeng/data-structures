1ffd74a781a57f6ac01dfc95437d98053b46b2a3
aitzeng <aitzeng@gmail.com>
2023-03-29 18:35:36 -0700

    Complete Prototypal instantiation Queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,25 @@
 var Queue = function() {
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[Object.keys(this.storage)[this.size() - 1] + 1] = value;
+};
+
+queueMethods.dequeue = function() {
+  var deletedStr = this.storage[Object.keys(this.storage)[0]];
+  delete this.storage[Object.keys(this.storage)[0]];
+  return deletedStr;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
 
 
